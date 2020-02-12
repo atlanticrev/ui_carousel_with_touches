f78b3214f33c4b0d73d35cccb5c47c7c0e6f6d83
Makc <atlanticrev@gmail.com>
2020-02-13 00:11:51 +0300

    velocity fix

--- script.js
+++ script.js
@@ -21,13 +21,14 @@ window.onload = () => {
     // const slides = slidesContainer.querySelectorAll('.slide');
 
     // Slider
-    const VELOCITY_FIX = 16.999;
+    const VELOCITY_FIX = 25;
     const VELOCITY_BOTTOM_THRESHOLD = 1;
     const FRICTION = 0.95;
 
     // Coordinates
     let startXPos = null;
     let currentXPos = null;
+    let offsetXPos = 0;
 
     // Offsets
     let currentOffset = 0;
@@ -49,48 +50,59 @@ window.onload = () => {
             return;
         // Применить трение, для остановки движения
         let velocityWithFriction = currentVelocity *= FRICTION;
-        logging();
+        // logging();
         // Остановить движение при очень маленькой скорости
         if (Math.abs(velocityWithFriction) < VELOCITY_BOTTOM_THRESHOLD) {
             isAnimate = false;
         }
         // Отобразить визульно вычисления
-        slidesContainer.style.setProperty('--offset', `${(lastOffset += velocityWithFriction)}px`);
+        slidesContainer.style.setProperty('--offset', `${(currentOffset = lastOffset += velocityWithFriction)}px`);
         requestAnimationFrame(animation);
     }
 
     function logging() {
-        // console.log('x-offset ->', currentXPos - startXPos);
-        // console.log('t-offset ->', offsetTime);
+        console.log('x-offset ->', currentXPos - startXPos);
+        console.log('t-offset ->', offsetTime);
         console.log('v-current ->', currentVelocity);
     }
 
     slidesContainer.ontouchstart = (e) => {
+        // Прекратить текущую анимацию
         isAnimate = false;
         startTime = Date.now();
+        // Стартовая координата в системе координат viewport-a
         startXPos = e.changedTouches[0].clientX;
     };
 
     slidesContainer.ontouchmove = (e) => {
+        // При смене направления скорости?
+
         currentTime = Date.now();
         // Общее время движения
         offsetTime = currentTime - startTime;
 
+        // Текущая координата в системе координат viewport-a
         currentXPos = e.changedTouches[0].clientX;
-        // Перемещение относительно прошлого смещения координаты
-        currentOffset = lastOffset + currentXPos - startXPos;
+
+        // Смещение относительно стартовой точки в начале движения
+        offsetXPos = currentXPos - startXPos;
+
+        // Перемещение относительно прошлого положения координаты
+        currentOffset = lastOffset + offsetXPos;
 
         // Как передать нужную скорость в rAF?
-        currentVelocity = ( currentXPos - startXPos ) / offsetTime * VELOCITY_FIX;
-        logging();
+        // Сколько пикселей за миллисекунду
+        currentVelocity = ( offsetXPos ) / offsetTime * VELOCITY_FIX;
         slidesContainer.style.setProperty('--offset', `${currentOffset}px`);
-        isAnimate = true;
     };
 
     slidesContainer.ontouchend = (e) => {
         // Определить мгновенную скорость в момент снятия touch
         lastOffset = currentOffset;
-        animation();
+        if (Math.abs(currentVelocity) >= 15) {
+            isAnimate = true;
+            animation();
+        }
     };
 
 };
